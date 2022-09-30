37bd01cbd2eedac6bd4f34a73fb7195173eb9bff
elijah <laughinglemur@protonmail.com>
2022-09-30 15:15:30 -0400

    Update Discord link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -117,7 +117,7 @@ const config = {
             items: [
               {
                 label: 'Discord',
-                href: 'https://discord.gg/4Jc3XhM5mp',
+                href: 'https://discord.com/invite/4tcveDjBuD',
               },
               {
                 label: 'Twitter',
